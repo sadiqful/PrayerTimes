21f9dae9a6a26345e2972e317313d130d5b49646
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-26 18:16:40 +0100

    double digit conditio

--- script.js
+++ script.js
@@ -26,4 +26,18 @@ if (hours > 12 ) {
 
 // Double digit condition
 
+if (hours<10){
+  hours = '0' + hours;
+}
+
+if (mins<10) {
+  mins = '0' + mins;
+}
+
+if (secs<10) {
+  secs = '0' + secs;
+}
+
+
+
 }
