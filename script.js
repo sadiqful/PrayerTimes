4694f2522e13ace683b63416099230f85ed1a195
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-26 18:14:27 +0100

    clockSystem conditions

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ function printTime() {
   var hours = day.getHours();
   var mins = day.getMinutes();
   var secs = day.getSeconds();
+  var clockSystem = "";
   var date = day.getDate();
   var month = day.getMonth();
   var year = day.getFullYear();
+
+// Am and PM condition
+
+if (hours < 12) {
+  clockSystem = "AM"
+} else {
+  clockSystem = "PM"
+}
+
+if (hours == 0) {
+  hours = 12;
+}
+
+if (hours > 12 ) {
+  hours = hours - 12;
+}
+
+// Double digit condition
+
 }
