f13eeff5813b416ae787c0145d7a74ab8f66fedf
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-26 18:08:44 +0100

    date and time data

---
+++ script.js
@@ -0,0 +1,9 @@
+function printTime() {
+  var day = new Date();
+  var hours = day.getHours();
+  var mins = day.getMinutes();
+  var secs = day.getSeconds();
+  var date = day.getDate();
+  var month = day.getMonth();
+  var year = day.getFullYear();
+}
